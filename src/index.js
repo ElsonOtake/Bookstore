0288e7bfe1ef78fd2c049cba535afbf05cccb769
Elson Otake <elsonotake@yahoo.com.br>
2022-07-07 18:22:54 -0300

    Add linter files and run linters

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-
-  </React.StrictMode>
+  <React.StrictMode />,
 );
-
