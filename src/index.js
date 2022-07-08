ca00f3a81f5f1c490a885fb0ca7137e11a56e978
Elson Otake <elsonotake@yahoo.com.br>
2022-07-08 09:22:35 -0300

    Add index.js with Routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Books from './components/Books';
+import Categories from './components/Categories';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode />,
+  <React.StrictMode>
+    <Router>
+      <Layout />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="categories" element={<Categories />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
 );
