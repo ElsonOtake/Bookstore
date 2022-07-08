957f7be496c128efa888882517436d16eba0a256
Elson Otake <elsonotake@yahoo.com.br>
2022-07-08 09:34:49 -0300

    Update single quotes

--- src/components/AddBooks.js
+++ src/components/AddBooks.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const AddBooks = () => (
   <form>
-    <input type='text' placeholder='Book title' />
-    <input type='text' placeholder='Author' />
-    <input type='submit' value='ADD BOOK' />
+    <input type="text" placeholder="Book title" />
+    <input type="text" placeholder="Author" />
+    <input type="submit" value="ADD BOOK" />
   </form>
 );
 
