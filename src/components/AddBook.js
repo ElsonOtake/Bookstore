190afcc3c784a03a97beffe48065ef6987f1e20a
Elson Otake <elsonotake@yahoo.com.br>
2022-07-08 12:37:01 -0300

    Add separated AddBook component

---
+++ src/components/AddBook.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddBook = () => (
+  <form>
+    <input type="text" placeholder="Book title" />
+    <input type="text" placeholder="Author" />
+    <input type="submit" value="ADD BOOK" />
+  </form>
+);
+
+export default AddBook;
