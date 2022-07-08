61f52732290fdae6a63f1bb4418fd7ebba58de8f
Elson Otake <elsonotake@yahoo.com.br>
2022-07-08 09:24:45 -0300

    Add Books component

---
+++ src/components/Books.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ListBooks from './ListBooks';
+import AddBooks from './AddBooks';
+
+const Books = () => {
+  return (
+    <>
+      <ListBooks />
+      <AddBooks />
+    </>
+  );
+}
+
+export default Books;
