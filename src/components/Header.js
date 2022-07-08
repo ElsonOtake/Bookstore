641c096d7ba66b574fa57da73a9dc8bdd6574876
Elson Otake <elsonotake@yahoo.com.br>
2022-07-08 09:45:28 -0300

    Update imports and arrow function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,22 @@
 import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
-const Header = () => {
-  return (
-    <>
-      <nav>
-        <h1>Bookstore CMS</h1>
-        <ul>
-          <li>
-            <Link to="/">BOOKS</Link>
-          </li>
-          <li>
-            <Link to="categories">CATEGORIES</Link>
-          </li>
-        </ul>
-      </nav>
+const Header = () => (
+  <>
+    <nav>
+      <h1>Bookstore CMS</h1>
+      <ul>
+        <li>
+          <Link to="/">BOOKS</Link>
+        </li>
+        <li>
+          <Link to="categories">CATEGORIES</Link>
+        </li>
+      </ul>
+    </nav>
 
-      <Outlet />
-    </>
-  )
-};
+    <Outlet />
+  </>
+);
 
 export default Header;
