9422506fd6389701604500f4f52fd45ab9f3ab77
Elson Otake <elsonotake@yahoo.com.br>
2022-07-11 15:40:03 -0300

    Add action creator

--- src/redux/categories/Categories.js
+++ src/redux/categories/Categories.js
@@ -4,6 +4,10 @@ const CHECK_STATUS = 'bookstore/categories/CHECK_STATUS';
 
 const initialState = [];
 
+export const checkStatus = () => ({
+  type: CHECK_STATUS,
+});
+
 const Categories = () => (
   <section>
     <button type="button">
