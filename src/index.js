e9d769375edf69f65f6c380c95f4fb30129805d8
Elson Otake <elsonotake@yahoo.com.br>
2022-07-11 19:08:07 -0300

    Add App.js to store index components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Books from './components/Books';
-import Categories from './components/Categories';
+import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="categories" element={<Categories />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
+  <App />,
 );
