1586c38d5dffa8cef0d7850792892ab0165f4c68
Elson Otake <elsonotake@yahoo.com.br>
2022-07-11 13:21:54 -0300

    Add action types

--- src/redux/books/Books.js
+++ src/redux/books/Books.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ListBooks from '../../components/ListBooks';
 import AddBook from '../../components/AddBook';
 
+const ADD = 'bookstore/books/ADD';
+const REMOVE = 'bookstore/books/REMOVE';
+
 const Books = () => (
   <>
     <ListBooks />
