d67e1c42effbb98860c315de5720189bdd79289f
Elson Otake <elsonotake@yahoo.com.br>
2022-07-11 20:31:11 -0300

    Update using selector

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
 const ListBooks = () => {
-  const booksData = [];
+  const booksData = useSelector((state) => state.booksReducer);
 
   return (
     <ul className="books_list">
