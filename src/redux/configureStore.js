812d08e82c2cad467fad289d0f090db8a80cc3ee
Elson Otake <elsonotake@yahoo.com.br>
2022-07-11 15:54:15 -0300

    Add combineReducers

---
+++ src/redux/configureStore.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import booksReducer from './books/Books';
+import statusReducer from './categories/Categories';
+
+const rootReducer = combineReducers({
+  booksReducer,
+  statusReducer
+});
+
+export default rootReducer;
