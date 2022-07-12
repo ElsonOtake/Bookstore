ebf49edd91cdcd6db6a567c6301a3ddf27ca5f87
Elson Otake <elsonotake@yahoo.com.br>
2022-07-12 15:37:49 -0300

    Add Remove componente in new file

---
+++ src/components/Remove.js
@@ -0,0 +1,24 @@
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/Books';
+
+const Remove = () => {
+  const dispatch = useDispatch();
+
+  const removeBookById = (event) => {
+    const { target } = event;
+    const id = target.parentNode.parentNode.firstChild.classList[0];
+    dispatch(removeBook(id));
+  };
+
+  return (
+    <button
+      type="button"
+      className="text_style_6"
+      onClick={removeBookById}
+    >
+      Remove
+    </button>
+  );
+};
+
+export default Remove;
