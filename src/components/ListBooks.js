fe8b42c0f2a3a3fa332524b2907e24d9b02f8759
Elson Otake <elsonotake@yahoo.com.br>
2022-07-13 15:47:40 -0300

    Update dispatch to api function

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
+import { retrieveBooks } from '../redux/books/Books';
 
 const ListBooks = () => {
   const booksData = useSelector((state) => state.booksReducer);
+  const dispatch = useDispatch();
+
+  dispatch(retrieveBooks())
+    .then((response) => {
+      console.log('ListBooks', response);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 
   return (
     <ul className="books_list">
