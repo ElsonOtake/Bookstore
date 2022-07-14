4c4ecf20920749dbbd8f772047037b2c7da3683c
Elson Otake <elsonotake@yahoo.com.br>
2022-07-14 18:37:17 -0300

    Add icon to nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,20 @@ import { Link, Outlet } from 'react-router-dom';
 const Header = () => (
   <>
     <nav>
-      <h1>Bookstore CMS</h1>
-      <ul>
-        <li>
-          <Link to="/">BOOKS</Link>
-        </li>
-        <li>
-          <Link to="categories">CATEGORIES</Link>
-        </li>
-      </ul>
+      <div>
+        <h1>Bookstore CMS</h1>
+        <ul>
+          <li>
+            <Link to="/">BOOKS</Link>
+          </li>
+          <li>
+            <Link to="categories">CATEGORIES</Link>
+          </li>
+        </ul>
+      </div>
+      <span className="material-icons-outlined">
+        person_outline
+      </span>
     </nav>
 
     <Outlet />
