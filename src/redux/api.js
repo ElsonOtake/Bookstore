b31e9eb03af8c42b2f172e1cc6c9a9fee3407946
Elson Otake <elsonotake@yahoo.com.br>
2022-07-14 10:17:49 -0300

    Update api output layout

--- src/redux/api.js
+++ src/redux/api.js
@@ -2,10 +2,23 @@ import axios from 'axios';
 
 const urlBooks = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/BluHDvdJs2fJBhr1nW5x/books';
 
+const changeFormat = (obj) => {
+  const response = [];
+  Object.entries(obj).forEach((data) => {
+    response.push({
+      id: data[0],
+      title: data[1].title,
+      author: data[1].author,
+      category: 'API',
+    });
+  });
+  return response;
+};
+
 export const axiosGet = async () => {
   try {
     const response = await axios.get(urlBooks);
-    return response.data;
+    return changeFormat(response.data);
   } catch (error) {
     throw new Error(error);
   }
