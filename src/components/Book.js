e730e89cef0fa5fb9649a214f1da5ab2f8032c08
Elson Otake <elsonotake@yahoo.com.br>
2022-07-14 17:01:09 -0300

    Update article JSX

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import Comments from './Comments';
 import Remove from './Remove';
 import Edit from './Edit';
 
 const Book = (props) => {
   const { id, title, author } = props;
+  const progress = Math.round(Math.random() * 100);
+  const chapter = Math.round(progress / 2 + 1);
   return (
     <article>
+      <p className="category text_style_5">Action</p>
       <h2 className={id}>{title}</h2>
       <p className="text_style_6">{author}</p>
       <span>
@@ -15,6 +20,20 @@ const Book = (props) => {
         <Remove />
         <Edit />
       </span>
+      <div className="progress_data">
+        <CircularProgressbar value={progress} />
+        <div className="completed_data">
+          <p className="completed">{`${progress}%`}</p>
+          <p>Completed</p>
+        </div>
+        <div className="chapter_data">
+          <p className="current">CURRENT CHAPTER</p>
+          <p className="chapter">{`Chapter ${chapter}`}</p>
+          <button type="button">
+            UPDATE PROGRESS
+          </button>
+        </div>
+      </div>
     </article>
   );
 };
